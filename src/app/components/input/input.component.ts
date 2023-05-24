6e4d225ab84e1df4d6550f09441a21015f65e1fe
SERAFIN <fagnerserafim31@gmail.com>
2023-05-24 15:16:43 -0300

    up project

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/item';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemQueVaiSerEditado!: Item
+
+  valorItem!: string;
+  constructor(
+    private listaService: ListaDeCompraService
+  ) { }
+
+  ngOnInit(): void { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(!changes['itemQueVaiSerEditado'].firstChange) {
+      this.valorItem = this.itemQueVaiSerEditado?.nome;
+    }
+  }
+
+  adicionarItem() {
+    this.listaService.adicionarItemNaLista(this.valorItem)
+    this.limparCampo();
+  }
+
+  limparCampo() {
+    this.valorItem = '';
+  }
+}
